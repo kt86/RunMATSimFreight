acbaa629d296fbbaa353a9304b9d225a5b08708f
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2025-08-15 14:51:00 +0200

    exceptions

--- src/main/java/org/matsim/freight/logistics/examples/multipleChains/RunStarterExampleTwoLspsGroceryDeliveryMultipleChainsWithToll.java
+++ src/main/java/org/matsim/freight/logistics/examples/multipleChains/RunStarterExampleTwoLspsGroceryDeliveryMultipleChainsWithToll.java
@@ -1,11 +1,17 @@
 package org.matsim.freight.logistics.examples.multipleChains;
 
+import org.matsim.core.config.CommandLine;
+
 /**
 * This class just forwards the main method to the
 * ExampleTwoLspsGroceryDeliveryMultipleChainsWithToll class.
 */
 public class RunStarterExampleTwoLspsGroceryDeliveryMultipleChainsWithToll {
     public static void main(String[] args) {
-        ExampleTwoLspsGroceryDeliveryMultipleChainsWithToll.main(args);
+        try {
+            ExampleTwoLspsGroceryDeliveryMultipleChainsWithToll.main(args);
+        } catch (CommandLine.ConfigurationException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
