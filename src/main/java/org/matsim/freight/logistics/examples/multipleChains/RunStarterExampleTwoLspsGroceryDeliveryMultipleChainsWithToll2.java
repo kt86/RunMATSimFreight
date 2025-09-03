f49287318c602e11f0d31f1fcfc03e88a6568bd4
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2025-09-03 11:40:28 +0200

    add another run script

---
+++ src/main/java/org/matsim/freight/logistics/examples/multipleChains/RunStarterExampleTwoLspsGroceryDeliveryMultipleChainsWithToll2.java
@@ -0,0 +1,41 @@
+package org.matsim.freight.logistics.examples.multipleChains;
+
+import org.matsim.core.config.CommandLine;
+
+import static org.matsim.freight.logistics.examples.multipleChains.ExampleTwoLspsGroceryDeliveryMultipleChainsWithToll.TypeOfLsps.ONE_PLAN_ONLY_DIRECT_CHAIN;
+
+/**
+* This class just forwards the main method to the
+* ExampleTwoLspsGroceryDeliveryMultipleChainsWithToll class.
+ * This class is used to run locally to try out certain cmdline arguments.*
+*/
+public class RunStarterExampleTwoLspsGroceryDeliveryMultipleChainsWithToll2 {
+    public static void main(String[] args) {
+        
+        try {
+            String[] argsToSet = {
+                    "--outputDirectory=outputKMT/testRun",
+                    "--matsimIterations=20",
+                    "--config:replanning.fractionOfIterationsToDisableInnovation=0.5",
+                    "--jspritIterationsDirect=10",
+                    "--jspritIterationsMain=1",
+                    "--jspritIterationsDistribution=10",
+                    "--tollValue=1000",
+                    "--tolledVehicleTypes=non",
+                    "--HubCostsFix=100",
+                    "--typeOfLsps=ONE_PLAN_BOTH_CHAINS",
+                    "--lsp1Name=Edeka_2chains_ICEV",
+                    "--lsp1CarrierId=edeka_SUPERMARKT_TROCKEN",
+                    "--lsp1HubLinkId=91085",
+                    "--lsp1vehTypesDirect=medium18t,heavy26t",
+                    "--lsp1vehTypesMain=heavy26t",
+                    "--lsp1vehTypesDelivery=medium18t,heavy26t"
+            };
+
+            ExampleTwoLspsGroceryDeliveryMultipleChainsWithToll.main(argsToSet);
+        } catch (CommandLine.ConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
